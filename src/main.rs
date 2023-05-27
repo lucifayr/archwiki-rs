208fa787c9229efbe8ab247f37f406a165cbbaa0
jackboxx <lucygschwantner@gmail.com>
2023-05-27 21:17:18 +0200

    Add support for basic tables

--- src/main.rs
+++ src/main.rs
@@ -174,26 +174,63 @@ async fn read_page(page: &str, pages: &[&str], show_urls: bool) -> Result<(), Wi
 fn format_children(node: NodeRef<Node>, show_urls: bool) -> String {
     match node.value() {
         Node::Text(text) => text.to_string(),
-        Node::Element(e) => {
-            let child_text = node
+        Node::Element(e) => match e.name() {
+            "a" => {
+                let child_text = node
+                    .children()
+                    .map(|node| format_children(node, show_urls))
+                    .collect::<Vec<String>>()
+                    .join("");
+
+                if show_urls {
+                    wrap_text_in_url(
+                        &child_text,
+                        &extract_tag_attr(e, &HtmlTag::A, "href").unwrap_or("".to_string()),
+                    )
+                } else {
+                    child_text
+                }
+            }
+            "tbody" | "tr" | "td" | "th" => node
+                .children()
+                .map(|node| format_table(node, show_urls))
+                .collect::<Vec<String>>()
+                .join(""),
+            _ => node
                 .children()
                 .map(|node| format_children(node, show_urls))
                 .collect::<Vec<String>>()
-                .join("");
-            if e.name() == "a" && show_urls {
-                wrap_text_in_url(
-                    &child_text,
-                    &extract_tag_attr(e, &HtmlTag::A, "href").unwrap_or("".to_string()),
-                )
-            } else {
-                child_text
-            }
-        }
+                .join(""),
+        },
         _ => node
             .children()
             .map(|node| format_children(node, show_urls))
             .collect::<Vec<String>>()
             .join(""),
+    }
+}
+
+fn format_table(node: NodeRef<Node>, show_urls: bool) -> String {
+    match node.value() {
+        Node::Text(text) => text.to_string().trim_end().to_owned(),
+        Node::Element(e) => match e.name() {
+            "tr" => {
+                node.children()
+                    .filter_map(|node| {
+                        let str = format_table(node, show_urls);
+                        if str.is_empty() {
+                            None
+                        } else {
+                            Some(format!("{str:<25}"))
+                        }
+                    })
+                    .collect::<Vec<String>>()
+                    .join(" | ")
+                    + "\n"
+            }
+            _ => format_children(node, show_urls),
+        },
+        _ => format_children(node, show_urls),
     }
 }
 
