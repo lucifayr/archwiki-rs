aaf4e59518ebc3f35855efb5a9ca9c2d501d4e93
jackboxx <lucygschwantner@gmail.com>
2023-09-08 12:43:10 +0200

    add tests for 'search' and 'list-languages' sub-commands

--- tests/cli.rs
+++ tests/cli.rs
@@ -69,3 +69,51 @@ fn test_cli_read_page_cmd() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[test]
+fn test_cli_search_cmd() -> Result<(), Box<dyn std::error::Error>> {
+    use predicate::str as pstr;
+
+    {
+        let mut cmd = Command::cargo_bin("archwiki-rs")?;
+        cmd.args(["search", "Neovim"]);
+
+        cmd.assert().success().stdout(pstr::starts_with(
+"PAGE                 | URL                                                                                       
+Neovim               | https://wiki.archlinux.org/title/Neovim"));
+    }
+
+    {
+        let mut cmd = Command::cargo_bin("archwiki-rs")?;
+        cmd.args(["search", "Neovim", "-L", "2"]);
+
+        cmd.assert().success().stdout(pstr::contains("\n").count(3));
+    }
+
+    {
+        let mut cmd = Command::cargo_bin("archwiki-rs")?;
+        cmd.args(["search", "Installation", "-t"]);
+
+        cmd.assert().success().stdout(
+            pstr::contains("\n")
+                .count(6)
+                .and(pstr::contains("PAGE                 | SNIPPET")),
+        );
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_cli_list_languages_cmd() -> Result<(), Box<dyn std::error::Error>> {
+    use predicate::str as pstr;
+
+    let mut cmd = Command::cargo_bin("archwiki-rs")?;
+    cmd.arg("list-languages");
+
+    cmd.assert()
+        .success()
+        .stdout(pstr::contains("en                   | English"));
+
+    Ok(())
+}
