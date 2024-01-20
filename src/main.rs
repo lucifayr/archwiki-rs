9903b978efd684a032107a3404545e24fea56bce
jackboxx <lucygschwantner@gmail.com>
2024-01-20 15:13:23 +0100

    split up functions in main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::fs;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 use clap::{CommandFactory, Parser};
 use clap_complete::{generate, Shell};
@@ -58,101 +61,14 @@ async fn main() -> Result<(), WikiError> {
     let default_page_file_path = data_dir.join(PAGE_FILE_NAME);
 
     match args.command {
-        Commands::ReadPage(ReadPageCliArgs {
-            page,
-            no_cache_write,
-            ignore_cache,
-            disable_cache_invalidation,
-            show_urls,
-            lang,
-            format,
-        }) => {
-            let page_cache_path = page_path(&page, &format, &cache_dir);
-            let use_cached_page = !ignore_cache
-                && page_cache_exists(&page_cache_path, disable_cache_invalidation).unwrap_or(false);
-
-            let out = if use_cached_page {
-                fs::read_to_string(&page_cache_path)?
-            } else {
-                match fetch_page(&page, lang.as_deref()).await {
-                    Ok(document) => match format {
-                        PageFormat::PlainText => convert_page_to_plain_text(&document, show_urls),
-                        PageFormat::Markdown => convert_page_to_markdown(&document, &page),
-                        PageFormat::Html => convert_page_to_html(&document, &page),
-                    },
-                    Err(err)
-                        if !ignore_cache
-                            && page_cache_exists(&page_cache_path, true).unwrap_or(false) =>
-                    {
-                        eprintln!("failed to fetch fresh page content, using possibly outdated cache instead\nERROR: {err}");
-                        fs::read_to_string(&page_cache_path)?
-                    }
-                    Err(err) => return Err(err),
-                }
-            };
-
-            let mut caching_failed_warning = String::new();
-
-            if !no_cache_write {
-                match fs::write(&page_cache_path, out.as_bytes()) {
-                    Ok(()) => {}
-                    Err(_) => {
-                        caching_failed_warning =
-                            format!("\n\n! failed to cache page with name {page}");
-                    }
-                }
-            }
-
-            println!("{out}{caching_failed_warning}");
+        Commands::ReadPage(args) => {
+            read_page(args, &cache_dir).await?;
         }
-        Commands::Search(SearchCliArgs {
-            search,
-            limit,
-            lang,
-            text_search,
-        }) => {
-            let out = if text_search {
-                let search_res = fetch_text_search(&search, &lang, limit).await?;
-                format_text_search_table(&search_res)
-            } else {
-                let search_res = fetch_open_search(&search, &lang, limit).await?;
-                let name_url_pairs = open_search_to_page_url_tupel(&search_res)?;
-                format_open_search_table(&name_url_pairs)
-            };
-
-            println!("{out}");
+        Commands::Search(args) => search_wiki(args).await?,
+        Commands::ListPages(args) => {
+            list_wiki_pages(args, default_page_file_path)?;
         }
-        Commands::ListPages(ListPagesCliArgs {
-            flatten,
-            categories,
-            page_file,
-        }) => {
-            let (path, is_default) =
-                page_file.map_or((default_page_file_path, true), |path| (path, false));
-
-            let wiki_tree = read_pages_file_as_category_tree(&path, is_default)?;
-            let out = list_pages(
-                &wiki_tree,
-                (!categories.is_empty()).then_some(&categories),
-                flatten,
-            );
-
-            println!("{out}");
-        }
-        Commands::ListCategories(ListCategoriesCliArgs { page_file }) => {
-            let (path, is_default) =
-                page_file.map_or((default_page_file_path, true), |path| (path, false));
-
-            let wiki_tree = read_pages_file_as_category_tree(&path, is_default)?;
-            let out = wiki_tree
-                .keys()
-                .unique()
-                .sorted()
-                .filter(|cat| cat.as_str() != UNCATEGORIZED_KEY)
-                .join("\n");
-
-            println!("{out}");
-        }
+        Commands::ListCategories(args) => list_wiki_categories(args, default_page_file_path)?,
         Commands::ListLanguages => {
             let langs = fetch_all_langs().await?;
             let out = format_lang_table(&langs);
@@ -195,13 +111,138 @@ async fn main() -> Result<(), WikiError> {
             )
             .await?;
         }
-        Commands::Info(InfoCliArgs {
-            show_cache_dir,
-            show_data_dir,
-            only_values,
-        }) => {
-            let no_flags_provided = !show_data_dir && !show_cache_dir;
-            let info = [
+        Commands::Info(args) => show_info(args, cache_dir, data_dir),
+        Commands::Completions(CompletionsCliArgs { shell }) => {
+            generate_shell_completion(shell.unwrap_or(Shell::from_env().expect(
+                "failed to automatically detect shell, please provided it as an explict argument",
+            )))
+        }
+    };
+
+    Ok(())
+}
+
+async fn read_page(
+    ReadPageCliArgs {
+        no_cache_write,
+        ignore_cache,
+        disable_cache_invalidation,
+        show_urls,
+        lang,
+        format,
+        page,
+    }: ReadPageCliArgs,
+    cache_dir: &Path,
+) -> Result<(), WikiError> {
+    let page_cache_path = page_path(&page, &format, cache_dir);
+    let use_cached_page = !ignore_cache
+        && page_cache_exists(&page_cache_path, disable_cache_invalidation).unwrap_or(false);
+
+    let out = if use_cached_page {
+        fs::read_to_string(&page_cache_path)?
+    } else {
+        match fetch_page(&page, lang.as_deref()).await {
+            Ok(document) => match format {
+                PageFormat::PlainText => convert_page_to_plain_text(&document, show_urls),
+                PageFormat::Markdown => convert_page_to_markdown(&document, &page),
+                PageFormat::Html => convert_page_to_html(&document, &page),
+            },
+            Err(err)
+                if !ignore_cache && page_cache_exists(&page_cache_path, true).unwrap_or(false) =>
+            {
+                eprintln!("failed to fetch fresh page content, using possibly outdated cache instead\nERROR: {err}");
+                fs::read_to_string(&page_cache_path)?
+            }
+            Err(err) => return Err(err),
+        }
+    };
+
+    let mut caching_failed_warning = String::new();
+
+    if !no_cache_write {
+        match fs::write(&page_cache_path, out.as_bytes()) {
+            Ok(()) => {}
+            Err(_) => {
+                caching_failed_warning = format!("\n\n! failed to cache page with name {page}");
+            }
+        }
+    }
+
+    println!("{out}{caching_failed_warning}");
+    Ok(())
+}
+
+async fn search_wiki(
+    SearchCliArgs {
+        search,
+        lang,
+        limit,
+        text_search,
+    }: SearchCliArgs,
+) -> Result<(), WikiError> {
+    let out = if text_search {
+        let search_res = fetch_text_search(&search, &lang, limit).await?;
+        format_text_search_table(&search_res)
+    } else {
+        let search_res = fetch_open_search(&search, &lang, limit).await?;
+        let name_url_pairs = open_search_to_page_url_tupel(&search_res)?;
+        format_open_search_table(&name_url_pairs)
+    };
+
+    println!("{out}");
+    Ok(())
+}
+
+fn list_wiki_pages(
+    ListPagesCliArgs {
+        flatten,
+        categories,
+        page_file,
+    }: ListPagesCliArgs,
+    default_page_file_path: PathBuf,
+) -> Result<(), WikiError> {
+    let (path, is_default) = page_file.map_or((default_page_file_path, true), |path| (path, false));
+
+    let wiki_tree = read_pages_file_as_category_tree(&path, is_default)?;
+    let out = list_pages(
+        &wiki_tree,
+        (!categories.is_empty()).then_some(&categories),
+        flatten,
+    );
+
+    println!("{out}");
+    Ok(())
+}
+
+fn list_wiki_categories(
+    ListCategoriesCliArgs { page_file }: ListCategoriesCliArgs,
+    default_page_file_path: PathBuf,
+) -> Result<(), WikiError> {
+    let (path, is_default) = page_file.map_or((default_page_file_path, true), |path| (path, false));
+
+    let wiki_tree = read_pages_file_as_category_tree(&path, is_default)?;
+    let out = wiki_tree
+        .keys()
+        .unique()
+        .sorted()
+        .filter(|cat| cat.as_str() != UNCATEGORIZED_KEY)
+        .join("\n");
+
+    println!("{out}");
+    Ok(())
+}
+
+fn show_info(
+    InfoCliArgs {
+        show_cache_dir,
+        show_data_dir,
+        only_values,
+    }: InfoCliArgs,
+    cache_dir: PathBuf,
+    data_dir: PathBuf,
+) {
+    let no_flags_provided = !show_data_dir && !show_cache_dir;
+    let info = [
                 (
                     !only_values,
                     "VALUE".into(),
@@ -222,32 +263,23 @@ async fn main() -> Result<(), WikiError> {
                 ),
             ];
 
-            let out = info
-                .iter()
-                .filter_map(|entry| {
-                    entry.0.then_some(if only_values {
-                        format!("{val}", val = entry.1.to_string_lossy())
-                    } else {
-                        format!(
-                            "{name:20} | {desc:90} | {val}",
-                            name = entry.2,
-                            desc = entry.3,
-                            val = entry.1.to_string_lossy()
-                        )
-                    })
-                })
-                .join("\n");
+    let out = info
+        .iter()
+        .filter_map(|entry| {
+            entry.0.then_some(if only_values {
+                format!("{val}", val = entry.1.to_string_lossy())
+            } else {
+                format!(
+                    "{name:20} | {desc:90} | {val}",
+                    name = entry.2,
+                    desc = entry.3,
+                    val = entry.1.to_string_lossy()
+                )
+            })
+        })
+        .join("\n");
 
-            println!("{out}");
-        }
-        Commands::Completions(CompletionsCliArgs { shell }) => {
-            generate_shell_completion(shell.unwrap_or(Shell::from_env().expect(
-                "failed to automatically detect shell, please provided it as an explict argument",
-            )))
-        }
-    }
-
-    Ok(())
+    println!("{out}");
 }
 
 fn generate_shell_completion(shell: Shell) {
