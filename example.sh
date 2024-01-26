d2dc652b7ed9a059e99cc7f35198931203d4db3c
jackboxx <lucygschwantner@gmail.com>
2024-01-26 11:46:49 +0100

    update example script

--- example.sh
+++ example.sh
@@ -1,7 +1,11 @@
-command=$HOME/.cargo/bin/archwiki-rs
-if output=$($command read-page $1 2>&1); then 
-    echo "$output" | less
-else
-    selection=$(echo "$output" | fzf) 
-    $command read-page "$selection" | less
+#!/bin/sh
+
+out=$(archwiki-rs read-page "$1" --format markdown 2>/tmp/awrs-recommendations) 
+
+if [ $? -eq 0 ]; then
+    echo "$out" | pandoc -s -f markdown -t man | man -p t -l -
+elif [ $(cat /tmp/awrs-recommendations | grep '[^[:space:]]' | wc -c) -ne 0 ]; then
+    selection=$(cat /tmp/awrs-recommendations | fzf) 
+    $0 "$selection"
 fi
+
