caa5fd0915627b283b9d053ef887a3bcaa5092c1
lucifayr <lucygschwantner@gmail.com>
2024-04-28 13:30:26 +0200

    wip: man page: check for top level man page

--- build.rs
+++ build.rs
@@ -18,19 +18,18 @@ fn main() {
     use clap::CommandFactory;
 
     let cmd = cmd::CliArgs::command();
-    let subcmds = cmd.get_subcommands().collect::<Vec<_>>();
-
+    let cmd_name = cmd.get_name();
+    let path = &format!("./man/man/{cmd_name}.1");
     debug_assert!(
-        subcmds.len() == 9,
-        "Amount of sub-commands has changed. Update the man pages as needed."
-    );
+            std::path::Path::new(path).exists(),
+            "The man page for the top level command '{cmd_name}' doesn't exists. Create a man page giving an overview of this tool. The man page should be stored at '{path}'");
 
+    let subcmds = cmd.get_subcommands().collect::<Vec<_>>();
     for cmd in subcmds {
         let cmd_name = cmd.get_name();
         let path = &format!("./man/man/archwik_rs-{cmd_name}.1");
         debug_assert!(
             std::path::Path::new(path).exists(),
-            "The man page for the command with the name '{cmd_name}' doesn't exists. Create a man page explaining what this command does and how to use it. The man page should be stored at '{path}'"
-        );
+            "The man page for the command with the name '{cmd_name}' doesn't exists. Create a man page explaining what this command does and how to use it. The man page should be stored at '{path}'");
     }
 }
